881a1e2b2372b348ffe933471d1611cec015439b
tk <tykeaboyloy@gmail.com>
2023-05-30 13:44:28 +0700

    close #23

--- pages/index.jsx
+++ pages/index.jsx
@@ -12,13 +12,24 @@ export default function Home() {
   const [signup, setSignup] = useRecoilState(signupState);
   const [loggedIn, setLoggedIn] = useRecoilState(loggedInState);
   const data = [
-    { title: 'football', description: 'This is card 1' ,image: 'Football.jpeg' },
-    { title: 'volleyball', description: 'This is card 2' ,image: 'Volleyball.jpeg'},
-    { title: 'golf', description: 'This is card 3' ,image: 'Golf.jpeg'},
-    { title: 'basketball', description: 'This is card 4' ,image: 'Basketball.jpeg'},
-    { title: 'tennis', description: 'This is card 6' ,image: 'Tennis.jpeg'},
+    {
+      title: "football",
+      description: "This is card 1",
+      image: "Football.jpeg",
+    },
+    {
+      title: "volleyball",
+      description: "This is card 2",
+      image: "Volleyball.jpeg",
+    },
+    { title: "golf", description: "This is card 3", image: "Golf.jpeg" },
+    {
+      title: "basketball",
+      description: "This is card 4",
+      image: "Basketball.jpeg",
+    },
+    { title: "tennis", description: "This is card 6", image: "Tennis.jpeg" },
   ];
-  
 
   function onClose() {
     setOpenLogin(false);
@@ -31,11 +42,13 @@ export default function Home() {
     localStorage.removeItem("user");
     setLoggedIn(false);
   }
-  return <div className={styles.container}>
-    <Hero/>
-    <CardSlider data={data} />
-    <br /> <br /> <br />
-    <FeatureProperties />
-    <Footer />
-  </div>;
+  return (
+    <div className={styles.container}>
+      <Hero />
+      <CardSlider data={data} />
+      <br /> <br /> <br />
+      <FeatureProperties />
+      <Footer />
+    </div>
+  );
 }
