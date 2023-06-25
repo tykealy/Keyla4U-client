acb354c0d463b9e2a4c79d7381b6d8791da1b751
tk <tykeaboyloy@gmail.com>
2023-06-25 18:26:47 +0700

    intergret booked page with apu

--- components/parent/BookedCard.jsx
+++ components/parent/BookedCard.jsx
@@ -4,134 +4,162 @@ import CardContent from "@mui/joy/CardContent";
 import CardOverflow from "@mui/joy/CardOverflow";
 import Typography from "@mui/joy/Typography";
 import useMediaQuery from "@mui/material/useMediaQuery";
-import Grid from '@mui/joy/Grid';
-import Button from '@mui/joy/Button';
+import Button from "@mui/joy/Button";
 import { useTheme } from "@mui/material/styles";
 import { Box } from "@mui/material";
-export default function BookedCard() {
+import Router from "next/router";
+import PropTypes from "prop-types";
+
+export default function BookedCard(props) {
+  const image = "http://admin.keyla4u.store/Keyla4U-server/public/img/";
+  const router = Router;
+  // const image = `${props.apiUrl}http://127.0.0/img/`;
+  // const image = `${props.apiUrl}/img/`;
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
   const isTablet = useMediaQuery(theme.breakpoints.down("md"));
+  const viewOnMap = (embeddedURL) => {
+    const normalURL = embeddedURL.replace("/maps/embed?", "/maps/?");
+    router.push(normalURL);
+  };
   return (
-    <Grid container spacing={2} sx={{ marginX:isMobile ? "0.3rem": "1rem"}}
+    <Card
+      orientation="horizontal"
+      variant="outlined"
+      sx={{
+        width: "100%",
+        height: isMobile ? "170px" : isTablet ? "200px" : "250px",
+        bgcolor: "background.body",
+      }}
     >
-     <Grid xs={12} md={6}>
-     <Card
-        orientation="horizontal"
-        variant="outlined"
+      <CardOverflow sx={{ objectFit: "cover" }}>
+        <AspectRatio
+          ratio="1"
+          sx={{
+            width: isMobile ? "170px" : isTablet ? "200px" : "250px",
+            padding: "5px",
+          }}
+        >
+          <img
+            src={`${image}${props.image}`}
+            loading="lazy"
+            alt="club's logo"
+          />
+        </AspectRatio>
+      </CardOverflow>
+      <CardContent
         sx={{
-          width: "100%",
-          height: isMobile ? "170px" : isTablet ? "200px" : "250px",
-          bgcolor: "background.body",
+          paddingLeft: isMobile ? "0.3rem" : "1.5rem",
+          paddingBottom: "1rem",
+          display: "flex",
         }}
       >
-        <CardOverflow sx={{ objectFit: "cover" }}>
-          <AspectRatio
-            ratio="1"
-            sx={{ width: isMobile ? "170px" : isTablet ? "200px" : "250px" , padding:"5px"}}
+        <Box
+          display={"flex"}
+          flexDirection={"column"}
+          gap={isMobile ? 0.3 : 0.5}
+        >
+          <Typography
+            component="p"
+            sx={{
+              fontWeight: "bold",
+              color: "teal",
+              fontSize: isMobile ? "0.6rem" : "1rem",
+            }}
           >
-            <img
-              src="https://images.unsplash.com/photo-1507833423370-a126b89d394b?auto=format&fit=crop&w=90"
-              srcSet="https://images.unsplash.com/photo-1507833423370-a126b89d394b?auto=format&fit=crop&w=90&dpr=2 2x"
-              loading="lazy"
-              alt=""
-            />
-          </AspectRatio>
-        </CardOverflow>
-        <CardContent sx={{ paddingLeft:isMobile ? "0.3rem"  : "1.5rem", paddingBottom:"1rem", display:"flex"}}>
-          <Box display={"flex"} flexDirection={"column"} gap={isMobile ? 0.3 : 0.5}>
-            <Typography component="p" sx={{ fontWeight: "bold" , color:"teal",fontSize:isMobile ? "0.6rem" : "1rem"}}>
-              Sport Center:
-            </Typography>
-            <Typography component="p" sx={{ fontWeight: "bold" , color:"teal",fontSize:isMobile ? "0.6rem" : "1rem"}}>
-              Court:
-            </Typography>
-            <Typography component="p" sx={{ fontWeight: "bold",color:"teal",fontSize:isMobile ? "0.6rem" : "1rem" }}>
-              Pitch:
-            </Typography>
-            <Typography component="p" sx={{ fontWeight: "bold",color:"teal",fontSize:isMobile ? "0.6rem" : "1rem" }}>
-              Booked Date:
-            </Typography>
-            <Typography component="p" sx={{ fontWeight: "bold", color:"teal",fontSize:isMobile ? "0.6rem" : "1rem" }}>
-              Play Date
-            </Typography>
-            <Typography component="p" sx={{ fontWeight: "bold",color:"teal",fontSize:isMobile ? "0.6rem" : "1rem" }}>
-              Time:
-            </Typography>
-            <Typography component="p" sx={{ fontWeight: "bold", color:"teal",fontSize:isMobile ? "0.6rem" : "1rem" }}>
-              Price:
-            </Typography>
-          </Box>
-           <Button
+            Sport Center: {props.name}
+          </Typography>
+          <Typography
+            component="p"
+            sx={{
+              fontWeight: "bold",
+              color: "teal",
+              fontSize: isMobile ? "0.6rem" : "1rem",
+            }}
+          >
+            Court: {props.court}
+          </Typography>
+          <Typography
+            component="p"
+            sx={{
+              fontWeight: "bold",
+              color: "teal",
+              fontSize: isMobile ? "0.6rem" : "1rem",
+            }}
+          >
+            Pitch: {props.pitch}
+          </Typography>
+          <Typography
+            component="p"
+            sx={{
+              fontWeight: "bold",
+              color: "teal",
+              fontSize: isMobile ? "0.6rem" : "1rem",
+            }}
+          >
+            Booked Date: {props.bookedDate}
+          </Typography>
+          <Typography
+            component="p"
+            sx={{
+              fontWeight: "bold",
+              color: "teal",
+              fontSize: isMobile ? "0.6rem" : "1rem",
+            }}
+          >
+            Play Date: {props.playDate}
+          </Typography>
+          <Typography
+            component="p"
+            sx={{
+              fontWeight: "bold",
+              color: "teal",
+              fontSize: isMobile ? "0.6rem" : "1rem",
+            }}
+          >
+            Time: {props.start_time} - {props.end_time}
+          </Typography>
+          <Typography
+            component="p"
+            sx={{
+              fontWeight: "bold",
+              color: "teal",
+              fontSize: isMobile ? "0.6rem" : "1rem",
+            }}
+          >
+            Price: {props.price}
+          </Typography>
+        </Box>
+        <Button
+          onClick={() => viewOnMap(props.map)}
           variant="solid"
           size="xs"
           aria-label="View on map"
-          sx={{ ml: 'auto',mb:"4rem",padding:"0.5rem", fontWeight: 600,bgcolor:"teal",fontSize: isMobile ? "0.6rem" : "1rem"}}
+          sx={{
+            ml: "auto",
+            mb: "4rem",
+            padding: "0.5rem",
+            fontWeight: 600,
+            bgcolor: "teal",
+            fontSize: isMobile ? "0.6rem" : "1rem",
+          }}
         >
           View on map
         </Button>
-        </CardContent>
-      </Card>
-      
-     </Grid>
-     <Grid xs={12} md={6}>
-     <Card
-        orientation="horizontal"
-        variant="outlined"
-        sx={{
-          width: "100%",
-          height: isMobile ? "170px" : isTablet ? "150px" : "250px",
-          bgcolor: "background.body",
-        }}
-      >
-        <CardOverflow sx={{ objectFit: "cover" }}>
-          <AspectRatio
-            ratio="1"
-            sx={{ width: isMobile ? "170px" : isTablet ? "150px" : "250px" ,padding:"5px"}}
-          >
-            <img
-              src="https://images.unsplash.com/photo-1507833423370-a126b89d394b?auto=format&fit=crop&w=90"
-              srcSet="https://images.unsplash.com/photo-1507833423370-a126b89d394b?auto=format&fit=crop&w=90&dpr=2 2x"
-              loading="lazy"
-              alt=""
-            />
-          </AspectRatio>
-        </CardOverflow>
-        <CardContent sx={{ paddingLeft:isMobile ? "0.3rem" : "1.5rem", paddingBottom:"1rem", display:"flex"}}>
-          <Box display={"flex"} flexDirection={"column"} gap={isMobile ? 0.3 : 0.5}>
-            <Typography component="p" sx={{ fontWeight: "bold" , color:"teal",fontSize:isMobile ? "0.6rem" : "1rem"}}>
-              Sport Center:
-            </Typography>
-            <Typography component="p" sx={{ fontWeight: "bold" , color:"teal",fontSize:isMobile ? "0.6rem" : "1rem"}}>
-              Court:
-            </Typography>
-            <Typography component="p" sx={{ fontWeight: "bold",color:"teal",fontSize:isMobile ? "0.6rem" : "1rem" }}>
-              Pitch:
-            </Typography>
-            <Typography component="p" sx={{ fontWeight: "bold",color:"teal",fontSize:isMobile ? "0.6rem" : "1rem" }}>
-              Booked Date:
-            </Typography>
-            <Typography component="p" sx={{ fontWeight: "bold", color:"teal",fontSize:isMobile ? "0.6rem" : "1rem" }}>
-              Play Date
-            </Typography>
-            <Typography component="p" sx={{ fontWeight: "bold",color:"teal",fontSize:isMobile ? "0.6rem" : "1rem" }}>
-              Time:
-            </Typography>
-            <Typography component="p" sx={{ fontWeight: "bold", color:"teal",fontSize:isMobile ? "0.6rem" : "1rem" }}>
-              Price:
-            </Typography>
-          </Box>
-          <Button
-          variant="solid"
-          size="xs"
-          aria-label="View on map"
-          sx={{ ml: 'auto',mb:"4rem",padding:"0.5rem", fontWeight: 600,bgcolor:"teal",fontSize: isMobile ? "0.6rem" : "1rem"}}
-        >
-          View on map
-        </Button>
-        </CardContent>
-      </Card>
-     </Grid>
-    </Grid>
+      </CardContent>
+    </Card>
   );
 }
+
+BookedCard.propTypes = {
+  image: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  court: PropTypes.string.isRequired,
+  pitch: PropTypes.string.isRequired,
+  bookedDate: PropTypes.string.isRequired,
+  playDate: PropTypes.string.isRequired,
+  start_time: PropTypes.string.isRequired,
+  end_time: PropTypes.string.isRequired,
+  price: PropTypes.string.isRequired,
+  map: PropTypes.string.isRequired,
+};
--- pages/booked/index.jsx
+++ pages/booked/index.jsx
@@ -1,18 +1,66 @@
-import * as React from 'react';
-import AspectRatio from '@mui/joy/AspectRatio';
-import Card from '@mui/joy/Card';
-import CardContent from '@mui/joy/CardContent';
-import CardOverflow from '@mui/joy/CardOverflow';
-import Typography from '@mui/joy/Typography';
+import * as React from "react";
 import BookedCard from "./../../components/parent/BookedCard";
-import Banner from '../../components/parent/Banner';
-import { Box } from '@mui/joy';
-const BookedPage = () => {
-   return(
+import Banner from "../../components/parent/Banner";
+import Grid from "@mui/joy/Grid";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { useTheme } from "@mui/material/styles";
+const BookedPage = (props) => {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+  const isTablet = useMediaQuery(theme.breakpoints.down("md"));
+  const [orders, setOrders] = React.useState([]);
+
+  React.useEffect(() => {
+    getUserOrders();
+  }, []);
+
+  const getUserOrders = async () => {
+    const user = JSON.parse(localStorage.getItem("user")).id;
+    const response = await fetch(`${props.apiUrl}/api/orders/${user}}`);
+    const data = await response.json();
+    setOrders(data);
+  };
+
+  React.useEffect(() => {
+    console.log(orders);
+  }, orders);
+  return (
     <div>
-        <Banner/>
-        <BookedCard />
+      <Banner />
+      <Grid
+        container
+        spacing={2}
+        sx={{ marginX: isMobile ? "0.3rem" : "1rem" }}
+      >
+        {orders.map((order, index) => {
+          return (
+            <Grid xs={12} md={6} key={index}>
+              <BookedCard
+                image={order.image}
+                name={order.name}
+                court={order.category_name}
+                pitch={order.pitch_num}
+                bookedDate={order.booked_date}
+                playDate={order.play_date}
+                start_time={order.start_time}
+                end_time={order.end_time}
+                price={order.total_amount}
+                map={order.map}
+              />
+            </Grid>
+          );
+        })}
+      </Grid>
     </div>
-   );
-}
+  );
+};
 export default BookedPage;
+
+export async function getServerSideProps(context) {
+  const apiUrl = process.env.API_URL;
+  return {
+    props: {
+      apiUrl: apiUrl,
+    },
+  };
+}
